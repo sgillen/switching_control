d54332fa0da0944e08e15016dd32d10a31d87b0a
Asutay Ozmen <ozmen@ucsb.edu>
2021-08-16 13:23:00 -0700

    run ppo

--- mat/runPPO.m
+++ mat/runPPO.m
@@ -1,5 +1,17 @@
 clear all;
-env = TreeClass;
-agent = rlPPOAgent(env.getObservationInfo,env.getActionInfo);
-opt = rlTrainingOptions('MaxEpisodes',1000,'MaxStepsPerEpisode',50);
+env = TreeClassCustomizable;
+initOpts = rlAgentInitializationOptions('NumHiddenUnit',64);
+agent = rlPPOAgent(env.getObservationInfo,env.getActionInfo,initOpts);
+%%%%%%%%%
+critic = getCritic(agent);
+critic.Options.LearnRate = 5*1e-3;
+critic.Options.UseDevice = 'cpu';
+agent  = setCritic(agent,critic);
+%%%%%%%%%%
+actor = getActor(agent);
+actor.Options.UseDevice = 'cpu';
+actor.Options.LearnRate = 1e-3;
+agent  = setActor(agent,actor);
+
+opt = rlTrainingOptions('MaxEpisodes',1000,'MaxStepsPerEpisode',60,'ScoreAveragingWindowLength',100);
 trainstats = train(agent, env, opt);
